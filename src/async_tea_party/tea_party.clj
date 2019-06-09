f6569e6275f04190e53ffcc8a461b67f48ab10e6
Justin D <justin@baal-software.com>
2019-06-09 11:11:05 -0600

    Uses buffered channel to manipulte I/O

--- src/async_tea_party/tea_party.clj
+++ src/async_tea_party/tea_party.clj
@@ -1,3 +1,12 @@
 (ns async-tea-party.tea-party
   (:require [clojure.core.async :as async]))
 
+(def tea-channel (async/chan 20))
+
+(async/>!! tea-channel :cup-of-tea)
+(async/>!! tea-channel :cup-of-tea-2)
+(async/>!! tea-channel :cup-of-tea-3)
+
+(async/<!! tea-channel)
+
+(async/close! tea-channel)
